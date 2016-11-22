0718c1c9058b7997de05c526f1fc6204f574795c
Chris van Marle <qistoph@gmail.com>
2016-11-22 18:30:45 +0100

    Add grunt and verification

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+	require("time-grunt")(grunt);
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		eslint: {
+			options: {
+				configFile: ".eslintrc.json"
+			},
+			target: ["*.js"]
+		},
+		stylelint: {
+			simple: {
+				options: {
+					configFile: ".stylelintrc"
+				},
+				src: ["*.css"]
+			}
+		},
+		jsonlint: {
+			main: {
+				src: ["package.json", ".eslintrc.json", ".stylelint"],
+				options: {
+					reporter: "jshint"
+				}
+			}
+		},
+		markdownlint: {
+			all: {
+				options: {
+					config: {
+						"default": true,
+						"line-length": false,
+						"blanks-around-headers": false,
+						"no-duplicate-header": false,
+						"no-inline-html": false,
+						"MD010": false,
+						"MD001": false,
+						"MD031": false,
+						"MD040": false,
+						"MD002": false,
+						"MD029": false,
+						"MD041": false,
+						"MD032": false,
+						"MD036": false,
+						"MD037": false,
+						"MD009": false,
+						"MD018": false,
+						"MD012": false,
+						"MD026": false,
+						"MD038": false
+					}
+				},
+				src: ["*.md"]
+			}
+		}
+	});
+	grunt.loadNpmTasks("grunt-eslint");
+	grunt.loadNpmTasks("grunt-stylelint");
+	grunt.loadNpmTasks("grunt-jsonlint");
+	grunt.loadNpmTasks("grunt-markdownlint");
+	grunt.registerTask("default", ["eslint", "stylelint", "jsonlint", "markdownlint"]);
+};
